21250bbf51a0265b69b65bc5e7742f36f92e3e97
oenos <oenos@free.fr>
2021-01-13 19:04:59 +0100

    la recursivité

--- app.js
+++ app.js
@@ -1,58 +1,23 @@
-// Calcules
-function addition(nbrA, nbrB) {
-    return nbrA + nbrB;
-}
+function timer(secondes){
 
-function multiplication(nbrA, nbrB) {
-    return nbrA * nbrB;
-}
-
-function soustration(nbrA, nbrB) {
-    return nbrA - nbrB;
-}
-
-function division(nbrA, nbrB) {
-    if(nbrB == 0){
-        throw new Error("Impossible de diviser par 0");
+    if(secondes > 0){
+        console.log(secondes);
+        timer(secondes - 1)
     }
-    return nbrA / nbrB;
-}
-
-
-
-do {
-    var choix = Number(prompt("Que souhaitez vous faire\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4- Division\n"));
-
-} while (choix != 1 && choix != 2 && choix != 3 && choix != 4)
-
-do {
-    var nbr1 = Number(prompt("Entrez un premier nombre"));
-    var nbr2 = Number(prompt("Entrez un deuxième nombre"));
-
-} while (isNaN(nbr1) || isNaN(nbr2))
-
-
-try {
-
-    switch (choix) {
-        case 1:
-            var resultat = addition(nbr1, nbr2);
-            break;
-        case 2:
-            var resultat = multiplication(nbr1, nbr2);
-            break;
-        case 3:
-            var resultat = soustration(nbr1, nbr2);
-            break;
-        case 4:
-            var resultat = division(nbr1, nbr2);
-            break;
-        default:
-            throw new Error("Une erreur est survenue.")
-
+    else {
+        console.log(secondes);
     }
-    alert(`Voici le résultat: ${resultat}`);
+
 }
-catch(error){
-    alert(error);
+
+timer(10);
+
+function somme(nbr){
+
+    if(nbr == 1){
+        return 1;
+    }
+    return nbr + somme(nbr - 1)
 }
+
+console.log(somme(3));
